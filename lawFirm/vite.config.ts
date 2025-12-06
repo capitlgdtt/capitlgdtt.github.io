39ee6ada15cedbcf704fe333fb8e80d8e73868b6
Vsovolod Grachev <vsevolod1607@bk.ru>
2025-12-06 18:03:51 +0300

    try to fix netlify #7

--- lawFirm/vite.config.ts
+++ lawFirm/vite.config.ts
@@ -2,16 +2,14 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react({
-      // Убираем React Compiler
-      jsxRuntime: 'automatic',
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
     }),
   ],
-  build: {
-    target: 'es2020',
-    sourcemap: false,
-  }
 })
